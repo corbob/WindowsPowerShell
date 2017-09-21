bf41a82763a3fc534db6fc14320680fbf23e4a68
corbob <corbobgithub@knoxy.ca>
2017-09-20 22:25:43 -0700

    Refactor to CIM instead of WMI

--- Functions/Get-PCInformation.ps1
+++ Functions/Get-PCInformation.ps1
@@ -3,24 +3,24 @@ Function Get-PCInformation {
         [String]$ComputerName,
         [pscredential]$Credential
     )
-    $cimSession = New-CimSession $ComputerName -SessionOption (New-CimSessionOption -Protocol DCOM) -OperationTimeoutSec 1 -ErrorAction SilentlyContinue -Credential $admin
+    $cimSession = New-CimSession $ComputerName -SessionOption (New-CimSessionOption -Protocol DCOM) -OperationTimeoutSec 1 -ErrorAction SilentlyContinue -Credential $Credential
     if (-not $cimSession) {
         Write-warning "$ComputerName is offline."
     } else {
-        $registry = Get-WmiObject -List "stdRegProv" -Namespace root\default -ComputerName $ComputerName
-        $ComputerSystem = Get-WmiObject -ComputerName $ComputerName -Class win32_computersystem
-        $OperatingSystem = Get-WmiObject win32_operatingsystem -ComputerName $ComputerName
-
-        $registry = Get-WmiObject -List "stdRegProv" -Namespace root\default -ComputerName $ComputerName
-        $version = $registry.GetStringValue(2147483650,"SOFTWARE\Microsoft\Windows NT\CurrentVersion","ReleaseID").svalue
-
+        $computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem -CimSession $cimSession
+        $OperatingSystem = Get-CimInstance -ClassName win32_operatingsystem -CimSession $cimSession
+        [UInt32]$HKLM = 2147483650
+        $registryKey = 'SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+        $registryValue = 'ReleaseID'
+        $version = (Invoke-CimMethod -Namespace root\default -ClassName StdRegProv -MethodName GetStringValue -Arguments @{hDefKey=$HKLM;sSubKeyName=$registryKey;sValueName=$registryValue} -CimSession $cimSession).sValue        
+        Remove-CimSession $cimSession
         if ($version -eq $null){
             $version = $OperatingSystem.version
         }
 
         [int]$memories = $ComputerSystem.TotalPhysicalMemory / 1GB
 
-        $uptime = $OperatingSystem.ConvertToDateTime($OperatingSystem.LocalDateTime) - $OperatingSystem.ConvertToDateTime($OperatingSystem.LastBootUpTime)
+        $uptime = $OperatingSystem.LocalDateTime - $OperatingSystem.LastBootUpTime
         $model = $ComputerSystem.SystemFamily
         if ($model -eq $null) {
             $model = $ComputerSystem.Model
@@ -35,7 +35,7 @@ Function Get-PCInformation {
             UpTime = "$($uptime.Days) days $($uptime.Hours) hours $($uptime.Minutes) minutes"
             OSCaption = $OperatingSystem.caption
             OSVersion = $version
-            BootTime = $OperatingSystem.ConvertToDateTime($OperatingSystem.LastBootUpTime)
+            BootTime = $OperatingSystem.LastBootUpTime
         }
         $obj = New-Object -TypeName psobject -Property $properties
         Write-Output $obj | Select-Object ComputerName,OSCaption,OSVersion,UserName,Model,Manufacturer,Memories,UpTime,BootTime
