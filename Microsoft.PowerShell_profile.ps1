a71fac5e70bc3c3ce0f858646ba34aad515e8763
corbob <corbobgithub@knoxy.ca>
2017-10-22 06:20:06 -0700

    Updated profile to *not* take 6+ seconds to load. Attempt to load modules and if that fails attempt to install module from PSGallery.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,25 @@
 . $PSScriptRoot\Private\Variables.ps1
-$availableModules = Get-Module -ListAvailable | Select-Object Name
 # Modules.csv contains 
 $modulesToImport = Import-Csv "$PSScriptRoot\Private\Modules.csv"
-ForEach ($module in $modulesToImport){
-    if ((-not $availableModules.Name.contains($module.Name)) -and ($module.InstallSource -eq 'PSGallery')) {
-        try {
-            Install-Module $module -Scope CurrentUser -ErrorAction Stop -AllowClobber
-            Import-Module $($module.Name) -ErrorAction Stop
-        } catch {
-            Write-Error "Could not install $($module.Name)"
-        }
-    }
+ForEach ($module in $modulesToImport) {
     try {
         Import-Module $($module.Name) -ErrorAction Stop
-    } catch {
-        Write-Error "Could not import $($module.name): $($_.Exception.Message)"
+    }
+    catch [System.IO.FileNotFoundException] {
+        if ($module.InstallSource -eq 'PSGallery') {
+            try {
+                Install-Module $module.Name -Scope CurrentUser -ErrorAction Stop -AllowClobber
+            }
+            catch [System.IO.FileNotFoundException] {
+                Write-Error "Could not import $($module.name): $($_.Exception.Message)"
+            }
+            catch {
+                Write-Error "Could not install $($module.Name): $($_.Exception.Message)"
+            }
+        }
+        else {
+            Write-Error "Could not import module $($module.name): Module not found."
+        }
     }
 }
 $Functions = Get-ChildItem $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue
@@ -27,3 +32,6 @@ foreach ($import in $Functions) {
     }
 }
 Set-PSReadlineKeyHandler Tab -Function MenuComplete
+Set-PowerLinePrompt -SetCurrentDirectory -RestoreVirtualTerminal -Newline -Timestamp -Colors "#000000" -PowerLineFont
+
+Set-Alias -Name mc -Value Measure-Command
