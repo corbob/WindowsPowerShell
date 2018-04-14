7592b18c655a5256ad262ff0634fdff05475e5e7
corbob <corbobgithub@knoxy.ca>
2018-04-14 06:21:00 -0700

    Restructure Profile. "Fix" Get-PCInformation...

--- Functions/Get-PCInformation.ps1
+++ Functions/Get-PCInformation.ps1
@@ -1,20 +1,22 @@
-Function MY-Get-PCInformation {
+Function Get-PCInformation {
     Param(
         [String]$ComputerName,
         [pscredential]$Credential
     )
     $cimSession = New-CimSession $ComputerName -SessionOption (New-CimSessionOption -Protocol DCOM) -OperationTimeoutSec 1 -ErrorAction SilentlyContinue -Credential $Credential
+    write-host "lkj: $cimsess"
     if (-not $cimSession) {
         Write-warning "$ComputerName is offline."
-    } else {
+    }
+    else {
         $computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem -CimSession $cimSession
         $OperatingSystem = Get-CimInstance -ClassName win32_operatingsystem -CimSession $cimSession
         [UInt32]$HKLM = 2147483650
         $registryKey = 'SOFTWARE\Microsoft\Windows NT\CurrentVersion'
         $registryValue = 'ReleaseID'
-        $version = (Invoke-CimMethod -Namespace root\default -ClassName StdRegProv -MethodName GetStringValue -Arguments @{hDefKey=$HKLM;sSubKeyName=$registryKey;sValueName=$registryValue} -CimSession $cimSession).sValue        
+        $version = (Invoke-CimMethod -Namespace root\default -ClassName StdRegProv -MethodName GetStringValue -Arguments @{hDefKey = $HKLM; sSubKeyName = $registryKey; sValueName = $registryValue} -CimSession $cimSession).sValue        
         Remove-CimSession $cimSession
-        if ($version -eq $null){
+        if ($version -eq $null) {
             $version = $OperatingSystem.version
         }
 
@@ -28,16 +30,16 @@ Function MY-Get-PCInformation {
 
         $properties = @{
             ComputerName = $ComputerName
-            UserName = $ComputerSystem.username
-            Model = $model
+            UserName     = $ComputerSystem.username
+            Model        = $model
             Manufacturer = $ComputerSystem.manufacturer
-            Memories = "$memories GB"
-            UpTime = "$($uptime.Days) days $($uptime.Hours) hours $($uptime.Minutes) minutes"
-            OSCaption = $OperatingSystem.caption
-            OSVersion = $version
-            BootTime = $OperatingSystem.LastBootUpTime
+            Memories     = "$memories GB"
+            UpTime       = "$($uptime.Days) days $($uptime.Hours) hours $($uptime.Minutes) minutes"
+            OSCaption    = $OperatingSystem.caption
+            OSVersion    = $version
+            BootTime     = $OperatingSystem.LastBootUpTime
         }
         $obj = New-Object -TypeName psobject -Property $properties
-        Write-Output $obj | Select-Object ComputerName,OSCaption,OSVersion,UserName,Model,Manufacturer,Memories,UpTime,BootTime
+        Write-Output $obj | Select-Object ComputerName, OSCaption, OSVersion, UserName, Model, Manufacturer, Memories, UpTime, BootTime
     }
 }
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,18 @@
-. $PSScriptRoot\Private\Variables.ps1
-# Modules.csv contains 
-$modulesToImport = Import-Csv "$PSScriptRoot\Private\Modules.csv"
+$PSDefaultParameterValues = @{
+    'Export-Csv:NoTypeInformation' = $true
+}
+$modulesToImport = @(
+    'AzureAD',
+    'AzureRM',
+    'MSOnline'
+)
 ForEach ($module in $modulesToImport) {
-    try {
-        Import-Module $($module.Name) -ErrorAction Stop
-    }
-    catch [System.IO.FileNotFoundException] {
-        if ($module.InstallSource -eq 'PSGallery') {
-            try {
-                Install-Module $module.Name -Scope CurrentUser -ErrorAction Stop -AllowClobber
-            }
-            catch [System.IO.FileNotFoundException] {
-                Write-Error "Could not import $($module.name): $($_.Exception.Message)"
-            }
-            catch {
-                Write-Error "Could not install $($module.Name): $($_.Exception.Message)"
-            }
-        }
-        else {
-            Write-Error "Could not import module $($module.name): Module not found."
-        }
+    $isValid = Get-Module $module -ListAvailable
+    if ($null -eq $isValid) {
+        Write-Warning "$module is not found in Modules path: $($env:PSModulePath)"
     }
 }
+
 $Functions = Get-ChildItem $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue
 foreach ($import in $Functions) {
     Try {
@@ -31,7 +22,7 @@ foreach ($import in $Functions) {
         Write-Error "Failed to import function $($import.fullname): $_"
     }
 }
+
 Set-PSReadlineKeyHandler Tab -Function MenuComplete
-Set-PowerLinePrompt -SetCurrentDirectory -RestoreVirtualTerminal -Newline -Timestamp -Colors "#000000" -PowerLineFont
 
 Set-Alias -Name mc -Value Measure-Command
