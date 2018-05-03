49215e5b69441c6d5985733e8701509a02af6c38
corbob <corbobgithub@knoxy.ca>
2018-05-03 06:16:56 -0700

    Made Get-PwnedPassword accept a Credential so you can test Credential objects

--- Functions/Get-PwnedPassword.ps1
+++ Functions/Get-PwnedPassword.ps1
@@ -1,43 +1,51 @@
 #http://jongurgul.com/blog/get-stringhash-get-filehash/ 
 Function Get-StringHash([String] $String,$HashName = "SHA1") 
 { 
-$StringBuilder = New-Object System.Text.StringBuilder 
-[System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))|%{ 
-[Void]$StringBuilder.Append($_.ToString("x2")) 
-} 
-$StringBuilder.ToString() 
+  $StringBuilder = New-Object System.Text.StringBuilder 
+  [System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))|%{ 
+  [Void]$StringBuilder.Append($_.ToString("x2")) 
+  } 
+  $StringBuilder.ToString() 
 }
 
 function Get-PwnedPassword {
 
     <#
-  .SYNOPSIS
-    Use the pwnedpassword API to check if entered password has been in a public data breach.
-  .DESCRIPTION
-    Use the pwnedpassword API to check if entered password has been in a public data breach.
-  .EXAMPLE
-    Get-PwnedPassword Password
-  .INPUTS
-    [String[]]$Password
-  .OUTPUTS
-    [String]
-  .NOTES
-    General notes
+    .SYNOPSIS
+      Use the pwnedpassword API to check if entered password has been in a public data breach.
+    .DESCRIPTION
+      Use the pwnedpassword API to check if entered password has been in a public data breach.
+    .EXAMPLE
+      Get-PwnedPassword Password
+    .INPUTS
+      [String]$Password
+    .OUTPUTS
+      [String]
+    .NOTES
+      General notes
   #>
 
     [CmdletBinding()]
     Param
     (
-        # Param1 help description
+        [Parameter( 
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName, 
+            ValueFromRemainingArguments, 
+            ParameterSetName = 'Password')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Password,
         [Parameter(Mandatory, 
             ValueFromPipeline,
             ValueFromPipelineByPropertyName, 
             ValueFromRemainingArguments, 
-            Position = 0)]
+            ParameterSetName = 'Credential')]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
-        [String[]]
-        $Password
+        [pscredential]
+        $Credential
     )
 
     Begin {
@@ -45,14 +53,18 @@ function Get-PwnedPassword {
     }
     Process {
         $count = 0
-        $SHA1 = Get-StringHash -String $Password
+        if ($Password) {
+            $ToCheck = $Password
+        }
+        if ($Credential) {
+            $ToCheck = $Credential.GetNetworkCredential().Password
+        }
+        $SHA1 = Get-StringHash -String $ToCheck
         $Result = Invoke-RestMethod -Uri "https://api.pwnedpasswords.com/range/$($SHA1[0..4] -join '')"
         $There = $Result -split "`r`n" | Where-Object { $PSItem -like "$($SHA1[5..$($SHA1.Length)] -join '')*" }
         if ($null -ne $There) {
             $count = ($There -split ':')[1]
         }
         Write-Output $count
-    }
-    End {
     }
 }
